5f8254058dbec70b454261e5785346af98d0346f
sebvautour <seb.vautour@hotmail.com>
2025-02-09 16:38:31 -0500

    chore: adjust openapi server

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -1,4 +1,4 @@
 import createClient from "openapi-fetch";
 import type { paths } from './api.generated';
 
-export default createClient<paths>({ baseUrl: import.meta.env.API_BASE_URL ?? 'http://localhost:8080/api/v1' });
+export default createClient<paths>({ baseUrl: import.meta.env.API_BASE_URL ?? '/api/v1' });
